5b47ff0c09a290343a4b043397f7bdb068955abb
chandan <elitegreen@gmail.com>
2018-05-31 02:17:19 +0200

    Added basic socket code to express web route setup to simulate a simple client server scenario

--- server/server.js
+++ server/server.js
@@ -1,14 +1,31 @@
 const path = require("path");
 const publicPath = path.join(__dirname, "../public");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
 app.use(express.static(publicPath));
 
+//Regsiter an EventListener and do something when the event takes place.
+
+//Handling connection with the client.
+io.on("connection", (socket) => {
+  console.log("Connected to client.");
+
+  //Handling disconnection from client.
+  socket.on("disconnect", () => {
+    console.log("Disconnected from client");
+  });
+});
+
 if(!module.parent) {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log(`Server started listenning to port: ${port}`);
   });
 }
